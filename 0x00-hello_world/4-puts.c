32696d76de1f58ea66a91104a826320d5865c0f7
fessycode <fessydoris@gmail.com>
2023-02-16 16:41:31 -0800

    4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdi.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
