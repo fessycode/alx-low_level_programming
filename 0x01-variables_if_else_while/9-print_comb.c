5b2e12ed92d6492d22213046f670fb5819429dea
fessycode <fessydoris@gmail.com>
2023-02-20 18:41:35 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,8 +15,11 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
