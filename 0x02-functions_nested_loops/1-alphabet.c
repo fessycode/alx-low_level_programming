f0c02627318485fc26c8b273f76d349656a74465
fessycode <fessydoris@gmail.com>
2023-02-22 13:39:59 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,17 +4,16 @@
 /**
  * print_alphabet - print the alphabet in lowercase
  *
- * Return: Always 0 (Success)
- *
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
 	char alph;
+
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
 		_putchar(alph);
 	}
 	_putchar('\n');
-	return;
 }
