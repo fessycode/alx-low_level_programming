ba2193d7d6e4862dac634ac6f5a7637f11800e97
fessycode <fessydoris@gmail.com>
2023-02-24 05:01:18 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_line - function that draws a straight line
+ * @n: is the integer for the paramaters of my function
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		if (n > 0)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
