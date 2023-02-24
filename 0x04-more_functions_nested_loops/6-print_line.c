ad6fa55b158861b0775cade1eeb7ccaf9c922279
fessycode <fessydoris@gmail.com>
2023-02-24 05:40:50 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,16 @@ void print_line(int n)
 {
 	int a;
 
-	for (a = 0; a <= n; a++)
+	if (n > 0)
 	{
-		if (n == 0 && n < 0)
-		{
-			_putchar('\n');
-		}
-		else
+		for (a = 0; a <= n; a++)
 		{
 			_putchar(95);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
