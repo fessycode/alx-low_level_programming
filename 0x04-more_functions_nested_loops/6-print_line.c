a53869896b3aef1f3f72d493d561878f2925438d
fessycode <fessydoris@gmail.com>
2023-02-24 05:43:11 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,22 @@
-#include "main.h"
-
-
+#include "holberton.h"
 /**
- * print_line - function that draws a straight line
+ * print_line - function that checks for uppercase character.
  * @n: is the integer for the paramaters of my function
  * Return: 0
  */
-
 void print_line(int n)
 {
-	int a;
+	int a = 0;
 
 	if (n > 0)
 	{
-		for (a = 0; a <= n; a++)
+		while (a <= n)
 		{
-			_putchar(95);
+			_putchar('_');
+			a++;
 		}
-		_putchar('\n');
+	 _putchar('\n');
 	}
 	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
