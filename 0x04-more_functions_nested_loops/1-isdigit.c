e9e4c4ba568faad6a9bdb651b654e0c8dd6a8761
fessycode <fessydoris@gmail.com>
2023-02-23 16:39:29 -0800

    1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if parameter is a digit.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 56)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
